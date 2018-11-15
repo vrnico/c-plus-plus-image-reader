c9017a88248e9342ed99a926eb3e5c88268af7ff
Nico Daunt <nico.daunt@gmail.com>
2018-11-14 16:54:27 -0800

    add image

---
+++ OpenCVTest/try.cpp
@@ -0,0 +1,15 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+int main()
+{
+	Mat img = imread("peace.jpg");
+	Mat photo = imread("photo.jpg");
+	namedWindow("image", WINDOW_NORMAL);
+	imshow("image", img);
+	namedWindow("photo", WINDOW_NORMAL);
+	imshow("photo", photo);
+	waitKey(0);
+	return 0;
+}
